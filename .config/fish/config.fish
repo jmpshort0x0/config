19ea47f6a873ad156caba6b865a0d5d8d83906f4
jmpshort0x0 <60113052+jmpshort0x0@users.noreply.github.com>
2023-05-11 12:38:53 -0700

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -55,3 +55,54 @@ function rmrf
     end
 end
 
+
+#utilizing fzf
+## https://github.com/SidOfc/dotfiles/tree/e94b96b908479950186e42a3709511a0afe300e4/.config/fish/functions
+function kp --description "Kill processes"
+  set -l __kp__pid (ps -ef | sed 1d | eval "fzf $FZF_DEFAULT_OPTS -m --header='[kill:process]'" | awk '{print $2}')
+  set -l __kp__kc $argv[1]
+
+  if test "x$__kp__pid" != "x"
+    if test "x$argv[1]" != "x"
+      echo $__kp__pid | xargs kill $argv[1]
+    else
+      echo $__kp__pid | xargs kill -9
+    end
+    kp
+  end
+end
+
+function kp --description "Kill processes"
+  set -l __kp__pid (ps -ef | sed 1d | eval "fzf $FZF_DEFAULT_OPTS -m --header='[kill:process]'" | awk '{print $2}')
+  set -l __kp__kc $argv[1]
+
+  if test "x$__kp__pid" != "x"
+    if test "x$argv[1]" != "x"
+      echo $__kp__pid | xargs kill $argv[1]
+    else
+      echo $__kp__pid | xargs kill -9
+    end
+    kp
+  end
+end
+
+function ks --description "Kill http server processes"
+  set -l __ks__pid (lsof -Pwni tcp | sed 1d | eval "fzf $FZF_DEFAULT_OPTS -m --header='[kill:tcp]'" | awk '{print $2}')
+  set -l __ks__kc $argv[1]
+
+  if test "x$__ks__pid" != "x"
+    if test "x$argv[1]" != "x"
+      echo $__ks__pid | xargs kill $argv[1]
+    else
+      echo $__ks__pid | xargs kill -9
+    end
+    ks
+  end
+end
+
+function reload --description "Reload fish shell"
+  if contains -- -f $argv
+    set -e __initialized
+  end
+  source ~/.config/fish/config.fish
+end
